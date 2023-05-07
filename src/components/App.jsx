2718ee0a8e5d1fb26370c0558f958700ab24f34a
Heorhii <gerythepatient@gmail.com>
2023-05-07 15:22:55 +0200

    Add first components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import { Component } from 'react';
+import { Section } from './Section/Section';
+import { NameForm } from './Section/NameForm/NameForm';
+import { ContactsList } from './Section/ContactsList/ContactsList';
+
+export class App extends Component {
+  
+  state = {
+    contacts: [],
+    name: '',
+  };
+  
+  handleNameChange = (event) => {
+  this.setState({name: event.currentTarget.value})
+  };
+
+  onAddContact = () => {
+    this.state.contacts.push({ id: 1, name: this.state.name });
+  }
+
+  render() {
+    return (
+     <div>
+        <Section title="Phonebook">
+         <NameForm name={this.state.name} handleNameChange={this.handleNameChange} onAddContact={this.onAddContact}></NameForm>
+        </Section>
+        <Section title="Contacts">
+          <ContactsList></ContactsList>
+        </Section>
+      </div>
   );
+  } 
 };
---
+++ src/components/Section/NameForm/NameForm.jsx
@@ -0,0 +1,17 @@
+export const NameForm = ({ name, handleNameChange, onAddContact }) => (
+    <form>
+        <label>Name <input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        value={name}
+        onChange={handleNameChange}
+        required
+        /></label>
+        <button type="button" onClick={onAddContact}>Add contact</button>
+    </form> 
+   
+)
+    
+   
